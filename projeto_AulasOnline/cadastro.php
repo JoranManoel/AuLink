f3d958ada23664bceec6cd296c077504106eb076
myllena-matias <myllebep@hotmail.com>
2016-08-12 13:20:14 -0300

    Update cadastro.php

--- projeto_AulasOnline/cadastro.php
+++ projeto_AulasOnline/cadastro.php
@@ -1,28 +1,56 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title>Cadastro realizado com sucesso!</title>
+</head>
+<body>
 <?php
 	try{
 			$conexao = new PDO("mysql:host=localhost; dbname=aulink","root","123");
 	}catch(PDOExeption $e){
-		echo "Código do Erro : ".$e->getCode()."Erro : ".$e->getMessage();
+		echo "CÃ³digo do Erro : ".$e->getCode()."Erro : ".$e->getMessage();
 	}
 	
 	try{
-		$stmt = $conexao->prepare("insert into usuario(nome, senha, login, email, sexo, nascimento) values(?,?,?,?,?,?)");
+		$stmt = $conexao->prepare("insert into usuario(nome, email, ddd, telefone, endereco, cidade, estado, bairro, pais, login, senha, news, sexo, nascimento) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
 
-		$nome=$_POST["nome"]; $senha=$_POST["senha"]; $login=$_POST["usuario"]; $email=$_POST["email"];
-		$sexo=$_POST["sexo"]; $nascimento=$_POST["nascimento"];
+		$nome= $_POST ["nome"];
+		$email= $_POST ["email"];
+		$ddd= $_POST ["ddd"];
+		$tel= $_POST ["telefone"];
+		$endereco= $_POST ["endereco"];
+		$cidade= $_POST ["cidade"];
+		$estado= $_POST ["estado"];
+		$bairro = $_POST ["bairro"];
+		$pais= $_POST ["pais"];
+		$login= $_POST ["login"];
+		$senha= $_POST ["senha"];
+		$news= $_POST ["news"];
+		$sexo= $_POST ["sexo"];
+		$sexo= $_POST ["nascimento"];
 
 		$stmt->bindParam(1,$nome);
-		$stmt->bindParam(2,$senha);
-		$stmt->bindParam(3,$login);
-		$stmt->bindParam(4,$email);
-		$stmt->bindParam(5,$sexo);
-		$stmt->bindParam(6,$nascimento);
+		$stmt->bindParam(2,$email);
+		$stmt->bindParam(3,$ddd);
+		$stmt->bindParam(4,$telefone);
+		$stmt->bindParam(5,$endereco);
+		$stmt->bindParam(6,$cidade);
+		$stmt->bindParam(7,$estado);
+		$stmt->bindParam(8,$bairro);
+		$stmt->bindParam(9,$pais);
+		$stmt->bindParam(10,$login);
+		$stmt->bindParam(11,$senha);
+		$stmt->bindParam(12,$newa);
+		$stmt->bindParam(13,$sexo);
+		$stmt->bindParam(14,$nascimento);
 
 		$stmt->execute();
 		echo("<script>alert('Cadastro realizado com sucesso!')</script>");
 		
 		
 	}catch(PDOEception $e){
-		echo "Houve erro na inserção.".$e->getCod()."-".$e->getMessage();	
+		echo "Houve erro na inserÃ§Ã£o.".$e->getCod()."-".$e->getMessage();	
 	}
 ?>
+</body>
+</html>
